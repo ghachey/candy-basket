0d267b0c748d58d1a241bd08c40f19ba48570952
Ghislain Hachey <ghachey@gmail.com>
2015-01-28 10:47:05 +0800

    feat(frontend): animations and other visuals improvements
    Improvements include:
    * slightly smoother loading experience
    * improvement of view switcher with state tracking
    * animations between views
    * styles improvements
    * all unit tests

--- frontend/app/scripts/services/state-tracker.js
+++ frontend/app/scripts/services/state-tracker.js
@@ -3,27 +3,26 @@
 /**
  * @ngdoc service
  * @name nasaraCandyBasketApp.stateTracker
- * @description 
- * # stateTracker 
- * Factory in the nasaraCandyBasketApp. This service can be used to share state
- * across various controllers' variables. It was born mainly because of the 
- * large impedance mismatch between Angular and TimelineJS which are hard to
- * get to work nice together in an Angular way. If this grows significantly 
- * overall design must be revised.
+ * @description # stateTracker Factory in the
+ * nasaraCandyBasketApp. This service can be used to share state
+ * across various controllers' variables. It was born mainly because
+ * of the large impedance mismatch between Angular and TimelineJS
+ * which are hard to get to work nice together in an Angular way. If
+ * this grows significantly overall design must be revised. It's also
+ * useful to carry state information across controllers
  */
 angular.module('nasaraCandyBasketApp')
   .factory('stateTracker', function () {
-    var state = {
-      timelineValues: {
+    var timelineValues = {
         index: 0, // i.e. current_slide index in TimelineJS
         /* jshint ignore:start */
         modal_open: false // this must go against convention...for now.
         /* jshint ignore:end */
-      }
-    };
+      };
 
     // Public API
     return {
-      state: state
+      timelineValues: timelineValues,
+      switcherView: 'candy-list-timeline'
     };
   });
